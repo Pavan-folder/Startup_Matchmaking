07e58d835a4da94a7b7596a3687d741af96615fc
Pavan <117636377+Pavan-folder@users.noreply.github.com>
2025-08-08 20:09:18 +0530

    Update and rename src/main.jsx to index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/globals.css"; // Optional, based on your setup
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
